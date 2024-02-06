50434f700d8075a60fdd10fb8f007261b94f5082
cjoyner150 <cjoyner150@gmail.com>
2024-02-05 22:21:27 -0500

    Updated character movement script
    Implemented a small amount of acceleration and a max speed to make movement feel slightly better. Implemented working ground check.

--- Assets/scripts/player movement.cs
+++ Assets/scripts/player movement.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [Header("Inscribed")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float maxJumpSpeed;
+    [SerializeField] private float speedMultiplier;
+    [SerializeField] private float jumpSpeedMultiplier;
+
     private Rigidbody2D body;
+
+    [Header("Dynamic")]
+    [SerializeField] private bool OnGround;
+
+
 
     private void Awake()
     {
@@ -12,9 +23,32 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        Vector2 Speed = new Vector2(body.velocity.x, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+
+        Speed.x += Time.deltaTime * speedMultiplier * Input.GetAxisRaw("Horizontal");
+
+
+        if (OnGround)
+        {
+            Speed.y = jumpSpeedMultiplier * Input.GetAxisRaw("Vertical");
+        }
+
+        Speed.x = Mathf.Clamp(Speed.x, -maxSpeed, maxSpeed);
+        Speed.y = Mathf.Clamp(Speed.y, -maxJumpSpeed, maxJumpSpeed);
+
+        body.velocity = Speed;
     }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 7) OnGround = true; // Layer 7 is ground
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 7) OnGround = false;
+    }
+
+
 }
