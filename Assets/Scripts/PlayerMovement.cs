857ebba1ecd69067b8512f5854ae8f9675dceb08
cjoyner150 <cjoyner150@gmail.com>
2024-02-13 13:46:26 -0500

    Expands player movement
    -Mag switch implemented (It simply uses 1, 2, 3, and 4 on the keyboard for now)
    -Testing scene/level cleaned up a little

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,8 +15,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float coyoteTime;
     [SerializeField] private bool conserveMomentum;
 
-    private float jumpDelay;
-    private float groundTime;
+    [Header("Dynamic")] // For ease of testing and debugging
+    [SerializeField] private float jumpDelay;
+    [SerializeField] private float groundTime;
+    [SerializeField] private int magSwitch = 0; // Zero is default (down), 1 is left, 2 is up, 3 is right
+
+    // We need to simulate our own gravity to change its direction
+    public Vector2 gravityDirection 
+    {
+        get 
+        {
+            if (magSwitch == 0) return new Vector2(0, -1);
+            else if (magSwitch == 1) return new Vector2(-1, 0);
+            else if (magSwitch == 2) return new Vector2(0, 1);
+            else return new Vector2(1, 0);
+        }
+    }
+    [SerializeField] private float gravityScale;
 
     private Rigidbody2D body;
     private BoxCollider2D coll;
@@ -31,12 +46,14 @@ public class PlayerMovement : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        body.gravityScale = 0;
     }
 
     private void Update()
     {
         #region INPUT HANDLER
         _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical"); // For wall running
         #endregion
         jumpDelay -= Time.deltaTime;
         groundTime -= Time.deltaTime;
@@ -45,32 +62,52 @@ public class PlayerMovement : MonoBehaviour
             jumpDelay = jumpBuffer;
         }
 
-        /*
-        Vector2 Speed = new Vector2(body.velocity.x, body.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // This way of handling mag switch input is most likely temporary
+        {
+            magSwitch = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            magSwitch = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            magSwitch = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            magSwitch = 3;
+        }
 
 
-        Speed.x += Time.deltaTime * speedMultiplier * Input.GetAxisRaw("Horizontal");
-
-        Speed.x = Mathf.Clamp(Speed.x, -maxSpeed, maxSpeed);
-        Speed.y = Mathf.Clamp(Speed.y, -maxJumpSpeed, maxJumpSpeed);
-
-        body.velocity = Speed;
-        */
     }
 
     private void FixedUpdate()
     {
         Run();
+        body.AddForce(gravityDirection * Physics2D.gravity.magnitude * gravityScale, ForceMode2D.Force);
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
-    }
+        if (magSwitch == 0) return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+        else if (magSwitch == 1) return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.left, .1f, jumpableGround);
+        else if (magSwitch == 2) return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.up, .1f, jumpableGround);
+        else return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.right, .1f, jumpableGround);
+    } // Returns a physics2d boxcast searching for jumpable ground in the direction of gravity
 
     private void Run()
     {
-        float targetSpeed = _moveInput.x * maxSpeed;
+        float targetSpeed;
+        if (magSwitch == 0 || magSwitch == 2)
+        {
+            targetSpeed = _moveInput.x * maxSpeed;
+        }
+        else
+        {
+            targetSpeed = _moveInput.y * maxSpeed;
+        }
+
 
         #region Calculate AccelRate
         float accelRate;
@@ -97,25 +134,57 @@ public class PlayerMovement : MonoBehaviour
         #endregion
 
         //Calculate difference between current velocity and desired velocity
-        float speedDif = targetSpeed - body.velocity.x;
+        float speedDif;
+        if (magSwitch == 0 || magSwitch == 2)
+        {
+            speedDif = targetSpeed - body.velocity.x;
+        }
+        else
+        {
+            speedDif = targetSpeed - body.velocity.y;
+        }
         //Calculate force along x-axis to apply to thr player
 
         float movement = speedDif*accelRate;
 
         //Convert this to a vector and apply to rigidbody
-        body.AddForce(movement * Vector2.right, ForceMode2D.Force);
         
-        if (groundTime>0 && jumpDelay>0 && body.velocity.y <= 0)
+        if (magSwitch == 0 || magSwitch == 2)
+        {
+            body.AddForce(movement * Vector2.right, ForceMode2D.Force);
+        }
+        else
+        {
+            body.AddForce(movement * Vector2.up, ForceMode2D.Force);
+        }
+
+        if (magSwitch == 0 && groundTime>0 && jumpDelay>0 && body.velocity.y <= 0)
         {
             body.velocity = new Vector2(body.velocity.x, jumpSpeedMultiplier);
             jumpDelay = 0;
         }
+        else if (magSwitch == 1 && groundTime>0 && jumpDelay>0 && body.velocity.x <= 0)
+        {
+            body.velocity = new Vector2(jumpSpeedMultiplier, body.velocity.y);
+            jumpDelay = 0;
+        }
+        else if (magSwitch == 2 && groundTime>0 && jumpDelay>0 && body.velocity.y >= 0)
+        {
+            body.velocity = new Vector2(body.velocity.x, -jumpSpeedMultiplier);
+            jumpDelay = 0;
+        }
+        else if (magSwitch == 3 && groundTime>0 && jumpDelay>0 && body.velocity.x >= 0)
+        {
+            body.velocity = new Vector2(-jumpSpeedMultiplier, body.velocity.y);
+            jumpDelay = 0;
+        }
+
         if (Input.GetButton("Jump"))
         {
-            body.gravityScale = lowGravMultiplier;
+            gravityScale = lowGravMultiplier;
         } else
         {
-            body.gravityScale = gravMultiplier;
+            gravityScale = gravMultiplier;
         }
     }
 
