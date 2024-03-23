f578355f066f1933d5c0a6c29f32f71a5a2a33f9
Syn-thetic <158788862+Syn-thetic@users.noreply.github.com>
2024-03-23 13:34:34 -0400

    Added timer to neutral jumps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private bool conserveMomentum;
     [SerializeField] private float downJumpReduction;
     [SerializeField] private float wallJumpSpeed;
+    [SerializeField] private float nairTime;
     [SerializeField] private float rotationSpeed;
 
     [SerializeField] private GameObject sprite;
@@ -195,7 +196,7 @@ public class PlayerMovement : MonoBehaviour
 
         #region Conserve Momentum
         //We won't slow the player down if they are moving in their desired direction but at a greater speed than their maxSpeed
-        if (((conserveMomentum && Mathf.Abs(body.velocity.x) > Mathf.Abs(targetSpeedX) && Mathf.Sign(body.velocity.x) == Mathf.Sign(targetSpeedX) && Mathf.Abs(targetSpeedX) > 0.01f)||_moveInput.x==0.0f) && !isGrounded)
+        if (((conserveMomentum && Mathf.Abs(body.velocity.x) > Mathf.Abs(targetSpeedX) && Mathf.Sign(body.velocity.x) == Mathf.Sign(targetSpeedX) && Mathf.Abs(targetSpeedX) > 0.01f)||_moveInput.x==0.0f && (surface % 4 >= 2 || surface % 16 >= 8)) && groundTime > -nairTime)
         {
             //Prevent any deceleration from happening, or in other words conserve are current momentum
             //You could experiment with allowing for the player to slightly increae their speed whilst in this "state"
